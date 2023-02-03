cf2ed186748b9dfeefce0b3ea85f65646e2d8409
DavidRakityanskiy <124116821+DavidRakityanskiy@users.noreply.github.com>
2023-02-02 16:41:26 -0800

    Create MathPrinter.java

---
+++ MathPrinter.java
@@ -0,0 +1,9 @@
+public class MathPrinter
+{
+   public static void main(String[] args)
+   {  
+            System.out.println("Hello");
+            System.out.println("My name is");
+            System.out.println("David");
+   }
+}
